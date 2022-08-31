cfc54b14d7f517a753d43edf4691f0c45bfb8203
janvi desai <janvi@webtheuniverse.com>
2022-08-31 16:12:17 +0530

    ADO.NET complete

---
+++ AdoDemo/AdoDemo/trial.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="trial.aspx.cs" Inherits="AdoDemo.trial" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="font-family:Arial">
+            <asp:Button ID="Get_Data" runat="server" Text="Get Data" OnClick="Get_Data_Click" />
+        &nbsp;<br />
+            <br />
+            <asp:GridView ID="gvStudent" runat="server" BackColor="White" BorderColor="#CCCCCC" BorderStyle="None" BorderWidth="1px" CellPadding="3" AutoGenerateColumns="False" DataKeyNames="act_id" OnRowCancelingEdit="gvStudent_RowCancelingEdit" OnRowDeleting="gvStudent_RowDeleting" OnRowEditing="gvStudent_RowEditing" OnRowUpdating="gvStudent_RowUpdating">
+                <Columns>
+                    <asp:CommandField ShowDeleteButton="True" ShowEditButton="True" />
+                    <asp:BoundField DataField="act_id" HeaderText="act_id" ReadOnly="True" SortExpression="act_id" />
+                    <asp:BoundField DataField="act_fname" HeaderText="act_fname" SortExpression="act_fname" />
+                    <asp:BoundField DataField="act_lname" HeaderText="act_lname" SortExpression="act_lname" />
+                    <asp:BoundField DataField="act_gender" HeaderText="act_gender" SortExpression="act_gender" />
+                </Columns>
+                <FooterStyle BackColor="White" ForeColor="#000066" />
+                <HeaderStyle BackColor="#006699" Font-Bold="True" ForeColor="White" />
+                <PagerStyle BackColor="White" ForeColor="#000066" HorizontalAlign="Left" />
+                <RowStyle ForeColor="#000066" />
+                <SelectedRowStyle BackColor="#669999" Font-Bold="True" ForeColor="White" />
+                <SortedAscendingCellStyle BackColor="#F1F1F1" />
+                <SortedAscendingHeaderStyle BackColor="#007DBB" />
+                <SortedDescendingCellStyle BackColor="#CAC9C9" />
+                <SortedDescendingHeaderStyle BackColor="#00547E" />
+            </asp:GridView>
+            <br />
+        </div>
+        <p>
+            &nbsp;</p>
+    </form>
+</body>
+</html>
---
+++ AdoDemo/AdoDemo/trial.aspx.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace AdoDemo
+{
+    public partial class trial : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void GetDataromDB()
+        {
+            string cs = "data source=JANVI-DESAI\\SQLEXPRESS; database=MOVIES_W3; Integrated Security=SSPI";
+            SqlConnection con = new SqlConnection(cs);
+            string strSelectQuery = "Select * from actor";
+            SqlDataAdapter da = new SqlDataAdapter(strSelectQuery, con);
+
+            DataSet ds = new DataSet();
+            da.Fill(ds, "Actor");
+
+            ds.Tables["Actor"].PrimaryKey = new DataColumn[] { ds.Tables["Actor"].Columns["act_id"] };
+            Cache.Insert("DATASET", ds, null, DateTime.Now.AddHours(24), System.Web.Caching.Cache.NoSlidingExpiration);
+
+            gvStudent.DataSource = ds;
+            gvStudent.DataBind();
+        }
+
+        private void GetDataFromCache()
+        {
+            if (Cache["DATASET"] != null)
+            {
+                DataSet ds = (DataSet)Cache["DATASET"];
+
+                gvStudent.DataSource = ds;
+                gvStudent.DataBind();
+            }
+        }
+
+        protected void gvStudent_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            if(Cache["DATASET"] != null)
+            {
+                DataSet ds = (DataSet)Cache["DATASET"];
+                DataRow dr = ds.Tables["Actor"].Rows.Find(e.Keys["act_id"]);
+                dr["act_fname"] = e.NewValues["act_fname"];
+                dr["act_lname"] = e.NewValues["act_lname"];
+                dr["act_gender"] = e.NewValues["act_gender"];
+
+                Cache.Insert("DATASET", ds, null, DateTime.Now.AddHours(24), System.Web.Caching.Cache.NoSlidingExpiration);
+                gvStudent.EditIndex = -1;
+                GetDataFromCache();
+            }
+        }
+
+        protected void gvStudent_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            gvStudent.EditIndex = -1;
+            GetDataFromCache();
+        }
+
+        protected void gvStudent_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            if (Cache["DATASET"] != null)
+            {
+                DataSet ds = (DataSet)Cache["DATASET"];
+                DataRow dr = ds.Tables["Actor"].Rows.Find(e.Keys["act_id"]);
+                dr.Delete();
+
+                Cache.Insert("DATASET", ds, null, DateTime.Now.AddHours(24), System.Web.Caching.Cache.NoSlidingExpiration);
+                GetDataFromCache();
+            }
+        }
+
+        protected void gvStudent_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            gvStudent.EditIndex = e.NewEditIndex;
+            GetDataFromCache();
+        }
+
+        protected void Get_Data_Click(object sender, EventArgs e)
+        {
+            GetDataromDB();
+        }
+    }
+}
